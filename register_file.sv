1288e7dd01aa9f6c1ba751da834fa77095a6c453
Ashwin Tyagi <ashwin.k.tyagi@gmail.com>
2023-11-06 23:39:50 +0000

    instantiated relevant modules in controller
    still does not work fully

--- register_file.sv
+++ register_file.sv
@@ -1,5 +1,5 @@
 module register_file (
-	input[1:0] reg1, reg2, reg_write, //addresses for the registers
+	input[1:0] reg1, reg2, reg_w, //addresses for the registers
 	input clk, reset, do_write,
 	input[7:0] write_data, //of size 8 for now but change if needed
 	output[7:0] data1, data2
@@ -24,7 +24,7 @@ module register_file (
 			registers[3] <= 8'b00000000;
 		end
 		else if(do_write)
-			registers[reg_write] <= write_data;
+			registers[reg_w] <= write_data;
 	
 	end
 	always@(negedge clk) begin
